d4ff31b0de09776dbd50a14c53e32756705f9ac1
abhishekrd <abhishekrdhanke636@gmail.com>
2023-05-21 14:09:00 +0530

    feat : Add Debouncing to optimize performance

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -8,30 +8,32 @@ const Search = () => {
   const [query, setQuery] = useState("");
   const [Movies, setMovies] = useState([]);
 
+  const getSearchedMovies = async (url) => {
+    try {
+      let res = await axios.get(url);
+      setMovies(res.data.results.splice(0, 8));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(
-    function () {
-      const upload = async () => {
-        await axios
-          .get(
-            `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${query}&page=1&include_adult=false`
-          )
-          .then((res) => {
-            setMovies(res.data.results.splice(0, 8));
-          })
-          .catch((e) => {
-            console.log(e);
-          });
-      };
-      upload();
-    },
-    [query, apiKey]
+    () => {
+      const timeOut = setTimeout(() => {
+        getSearchedMovies(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${query}&page=1&include_adult=false`);
+
+      }, 600)
+
+      return () => clearTimeout(timeOut);
+    }
+    , [query, apiKey]
   );
 
   return (
     <div className={`${styles.boxWidth} px-4 my-8`}>
       <div className="flex gap-8 items-center">
         <h1 className="text-2xl text-white">Search: </h1>
-        <input type="search" placeholder="Search" onChange={e => setQuery(e.target.value)} className="rounded border-none ring-1 ring-blue-300 outline-none"/>
+        <input type="search" placeholder="Search" onChange={e => setQuery(e.target.value)} className="rounded border-none ring-1 ring-blue-300 outline-none" />
       </div>
       {query && <div className={`${styles.boxWidth} my-8`}>
         <div
