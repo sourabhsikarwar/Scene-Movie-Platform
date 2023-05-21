c3ea882454cbae8f3a4de4e526bf65d3647fad57
Saksham Awasthi <sakshamawasthi1111@gmail.com>
2023-05-21 18:21:52 +0530

    Added the google auth at login page
    Added the login functionality and also configured it with the firebaseconfig.js

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore"
-import { getAuth } from 'firebase/auth'
+import { getAuth , GoogleAuthProvider } from 'firebase/auth'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,5 +21,6 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
 export const database = getFirestore(app);
 export const analytics = getAnalytics(app);
