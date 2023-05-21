111efc80608acba384898bc736e21ce0a9ab0796
sathvik K <sathvik902@gmail.com>
2023-05-21 13:03:33 +0530

    Added Routes

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Footer from "./components/Footer";
 import Signup from "./components/Signup";
 import Profile from "./components/Profile";
 import Home from "./pages/Home";
+import About from './pages/About';
+import TvShows from "./pages/TvShows";
 import Movie from "./pages/Movie";
 import Categories from "./pages/Categories";
 import { UserAuthContextProvider } from "./context/authContext";
@@ -23,6 +25,20 @@ function App() {
               <ProtectedRoute>
                 <Home />
               </ProtectedRoute>
+            }
+          />
+          <Route
+            exact
+            path="/about"
+            element={
+                <About />
+            }
+          />
+          <Route
+            exact
+            path="/tvshows"
+            element={
+                <TvShows />
             }
           />
           <Route
