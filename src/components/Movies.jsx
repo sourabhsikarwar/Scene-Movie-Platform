fa19762c1e53b72841a29e8afcf0a2bfca6cccfa
Suzy <sulagnaghosh52@gmail.com>
2023-05-25 02:32:13 +0530

    fix: solve trending page loading issue

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,68 +1,86 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import { Oval } from "react-loader-spinner";
-import Pagination from "./Pagination";
-import Card from "./Cards/Card";
-import styles from "../style"
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { Oval } from 'react-loader-spinner'
+import Pagination from './Pagination'
+import Card from './Cards/Card'
+import styles from '../style'
 
 function Movies(props) {
   const apiKey = process.env.REACT_APP_API_KEY
-  const [Movies, setMovies] = useState([]);
-  const [page, setPage] = useState(1);
+  const [initialLoading, setInitialLoading] = useState(false)
+  const [Movies, setMovies] = useState([])
+  const [page, setPage] = useState(1)
   const goBack = () => {
     if (page > 1) {
-      setPage(page - 1);
+      setPage(page - 1)
     }
-  };
+  }
   const goNext = () => {
-    setPage(page + 1);
-  };
+    setPage(page + 1)
+  }
+  useEffect(() => {
+    upload()
+  }, [page])
 
-  useEffect(function () {
-    const upload = async () => {
-      await axios
+  const upload = async () => {
+    setInitialLoading(true)
+    await axios
       .get(
         `https://api.themoviedb.org/3/discover/${props.content}?api_key=${apiKey}&with_genres=${props.id}&page=${page}`
       )
       .then((res) => {
-        setMovies(res.data.results);
+        if (res.status === 200) {
+          setMovies(res.data.results)
+          setInitialLoading(false)
+        }
       })
-      .catch( (e) => {
-        console.log(e);
-      });
-    }
-    upload()
-  }, [page, props, apiKey]);
-
+      .catch((e) => {
+        console.log(e)
+      })
+  }
   return (
     <>
-      <div className={`${styles.boxWidth} my-8`}>
-        <div className={`${styles.heading2} w-full my-2 ml-0 px-4 text-center sm:text-left`}>
-          {props.title}
+      {!initialLoading ? (
+        <>
+          {' '}
+          <div className={`${styles.boxWidth} my-8`}>
+            <div
+              className={`${styles.heading2} w-full my-2 ml-0 px-4 text-center sm:text-left`}
+            >
+              {props.title}
+            </div>
+            {Movies.length === 0 ? (
+              <div
+                className={`${styles.heading2} w-full my-2 ml-0 px-4 text-center sm:text-center`}
+              >
+                No Movie
+              </div>
+            ) : (
+              <>
+                {' '}
+                <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 sm:justify-between justify-center flex-wrap my-4 mx-auto'>
+                  {Movies.map((movie) => {
+                    return <Card movie={movie} />
+                  })}
+                </div>
+                <Pagination page={page} goBack={goBack} goNext={goNext} />
+              </>
+            )}
+          </div>
+        </>
+      ) : (
+        <div className='flex justify-center my-8'>
+          <Oval
+            height='50'
+            width='50'
+            color='grey'
+            secondaryColor='grey'
+            ariaLabel='loading'
+          />
         </div>
-        {Movies.length === 0 ? (
-          <div className="flex justify-center my-8">
-            <Oval
-              height="50"
-              width="50"
-              color="grey"
-              secondaryColor="grey"
-              ariaLabel="loading"
-            />
-          </div>
-        ) : (
-          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 sm:justify-between justify-center flex-wrap my-4 mx-auto">
-            {Movies.map((movie) => {
-              return (
-                <Card movie={movie}/>
-              );
-            })}
-          </div>
-        )}
-      </div>
-      <Pagination page={page} goBack={goBack} goNext={goNext} />
+      )}
     </>
-  );
+  )
 }
 
-export default Movies;
+export default Movies
