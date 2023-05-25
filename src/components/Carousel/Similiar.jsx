fde6db0165c1fa3799ba8b04f323bae17bfee53f
Suzy <sulagnaghosh52@gmail.com>
2023-05-25 18:50:41 +0530

    feat: add loader when data is loading

--- src/components/Carousel/Similiar.jsx
+++ src/components/Carousel/Similiar.jsx
@@ -1,76 +1,97 @@
-import { Splide, SplideSlide } from "@splidejs/react-splide";
-import "@splidejs/react-splide/css";
-import axios from "axios";
-import React, { useState, useEffect, useRef } from "react";
-import styles from "../../style";
-import MovieCard from "../Cards/MovieCard";
+import { Splide, SplideSlide } from '@splidejs/react-splide'
+import '@splidejs/react-splide/css'
+import axios from 'axios'
+import React, { useState, useEffect, useRef } from 'react'
+import styles from '../../style'
+import MovieCard from '../Cards/MovieCard'
+import { Oval } from 'react-loader-spinner'
 
 const Similiar = (props) => {
   const apiKey = process.env.REACT_APP_API_KEY
-  const [data, setData] = useState([]);
-  const isMounted = useRef(true);
+  const [initialLoading, setInitialLoading] = useState(true)
+  const [data, setData] = useState([])
+  const isMounted = useRef(true)
   const upload = async () => {
+    setInitialLoading(true)
     await axios
-      .get(`https://api.themoviedb.org/3/${props.title}/${props.id}/similar?api_key=${apiKey}&language=en-US&page=1`
+      .get(
+        `https://api.themoviedb.org/3/${props.title}/${props.id}/similar?api_key=${apiKey}&language=en-US&page=1`
       )
       .then((res) => {
-        setData(res.data.results);
+        if (res.statusCode === 200) {
+          setData(res.data.results)
+          setInitialLoading(false)
+        }
       })
       .catch((e) => {
-        console.log(e.message);
-      });
-  };
+        console.log(e.message)
+      })
+  }
 
   useEffect(function () {
     if (isMounted.current) {
-      upload();
+      upload()
     }
     return () => {
-      isMounted.current = false;
-    };
-  });
+      isMounted.current = false
+    }
+  })
 
   return (
-    <div className={`${styles.boxWidth} my-8`}>
-      <div className="flex justify-between items-center px-4">
-        <h2 className={`${styles.heading3}`}>Similiar</h2>
-      </div>
-      <Splide
-        options={{
-          type: "loop",
-          perPage: "6",
-          pagination: false,
-          breakpoints: {
-            400: {
-              perPage: 2,
-            },
-            764: {
-              perPage: 3,
-            },
-            1024: {
-              perPage: 4,
-            },
-            1280: {
-              perPage: 5,
-            },
-            1400: {
-              perPage: 6,
-            },
-          },
-        }}
-        aria-label="My Favorite Images"
-        className="justify-center"
-      >
-        {data.map((item) => {
-          return (
-            <SplideSlide>
-              <MovieCard movie={item} key={item.id} />
-            </SplideSlide>
-          );
-        })}
-      </Splide>
-    </div>
-  );
-};
+    <>
+      {initialLoading ? (
+        <div className='flex justify-center my-8'>
+          <Oval
+            height='50'
+            width='50'
+            color='grey'
+            secondaryColor='grey'
+            ariaLabel='loading'
+          />
+        </div>
+      ) : (
+        <div className={`${styles.boxWidth} my-8`}>
+          <div className='flex justify-between items-center px-4'>
+            <h2 className={`${styles.heading3}`}>Similiar</h2>
+          </div>
+          <Splide
+            options={{
+              type: 'loop',
+              perPage: '6',
+              pagination: false,
+              breakpoints: {
+                400: {
+                  perPage: 2,
+                },
+                764: {
+                  perPage: 3,
+                },
+                1024: {
+                  perPage: 4,
+                },
+                1280: {
+                  perPage: 5,
+                },
+                1400: {
+                  perPage: 6,
+                },
+              },
+            }}
+            aria-label='My Favorite Images'
+            className='justify-center'
+          >
+            {data.map((item) => {
+              return (
+                <SplideSlide>
+                  <MovieCard movie={item} key={item.id} />
+                </SplideSlide>
+              )
+            })}
+          </Splide>
+        </div>
+      )}
+    </>
+  )
+}
 
-export default Similiar;
+export default Similiar
