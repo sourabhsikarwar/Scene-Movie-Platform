6b2f0cd342349636944b237defcc48f8e0b52a87
karananu <karanrana3095@gmail.com>
2023-05-31 14:16:34 +0530

    addeda staticcrousel

---
+++ src/components/Carousel/Corouse.jsx
@@ -0,0 +1,79 @@
+import React from 'react'
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+const Corouse = () => {
+  return (
+    <Carousel autoPlay>
+    <div>
+      <img alt="" src="https://cdn.wallpapersafari.com/63/91/EGjUcK.jpg" />
+      
+    </div>
+    <div>
+      <img alt="" src="https://images.hindustantimes.com/img/2023/01/25/1600x900/pathaan_movie_review_1674631292696_1674631292838_1674631292838.jpeg" />
+     
+    </div>
+    <div>
+      <img alt="" src="https://e1.pxfuel.com/desktop-wallpaper/588/215/desktop-wallpaper-exists-horror-movie-data-src-horror-movie-backgrounds-hollywood-movie-poster.jpg" />
+      
+    </div>
+    <div>
+      <img alt="" src="https://akm-img-a-in.tosshub.com/indiatoday/images/story/202210/varun_bhediya-sixteen_nine.jpg?VersionId=7RC4rAD6WABhaTVnQ0F3i30Ttfqq4K97&size=690:388" />
+      
+    </div>
+    <div>
+      <img alt="" src="https://www.appocalypse.co/wp-content/uploads/2019/08/War-Movie-Poster-Hindi-1024x682.jpg" />
+      
+    </div>
+    <div>
+      <img alt="" src="https://www.xfire.com/wp-content/uploads/2023/03/fast-x-cover-3-768x382.jpg.webp" />
+     
+    </div>
+    <div>
+      <img alt="" src="https://www.whoa.in/download/london-hass-fallen-2016-hollywood-upcoming-movies-poster" />
+    
+    </div>
+    <div>
+      <img alt="" src="https://wallpapercave.com/wp/wp10822583.jpg" />
+      
+    </div>
+    <div>
+      <img alt="" src="https://static.toiimg.com/photo/39691811.cms" />
+   
+    </div>
+    <div>
+      <img alt="" src="https://wallpaperaccess.com/full/1512323.jpg" />
+      
+    </div>
+    <div>
+      <img alt="" src="https://wallpaperaccess.com/full/1076854.jpg" />
+      
+    </div>
+    <div>
+      <img alt="" src="https://c4.wallpaperflare.com/wallpaper/698/746/448/action-adventure-fantasy-film-wallpaper-preview.jpg" />
+      
+    </div>
+    <div>
+      <img alt="" src="https://c4.wallpaperflare.com/wallpaper/726/46/535/movies-hollywood-movies-wallpaper-preview.jpg" />
+    
+    </div>
+    <div>
+      <img alt="" src="https://cdn.wallpapersafari.com/53/79/ajwtby.jpg" />
+      
+    </div>
+    <div>
+      <img alt="" src="https://p4.wallpaperbetter.com/wallpaper/364/270/424/movies-hollywood-movies-wallpaper-preview.jpg" />
+      
+    </div>
+    <div>
+      <img alt="" src="https://cdn.shopify.com/s/files/1/0969/9128/products/BeautyAndTheBeast-LiveAction-HollywoodEnglishMoviePoster_51eb56f7-b32a-4288-817e-4c25e5db91ca.jpg?v=1625220672" />
+      
+    </div>
+    <div>
+      <img alt="" src="https://cdn.shopify.com/s/files/1/0969/9128/products/LordOfTheRings-FellowshipOfTheRing-HollywoodMoviePoster_ab38af96-9cf9-4611-b4d9-c45474e257f0.jpg?v=1630764631" />
+      
+    </div>
+  </Carousel>
+  )
+}
+
+export default Corouse
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,64 +1,12 @@
-import React, { useState, useRef, useEffect } from 'react'
-import axios from 'axios'
-import Banner from '../components/Banner/Banner'
-import Trending from '../components/Carousel/Trending'
-import Search from '../components/Search'
-import { Oval } from 'react-loader-spinner'
+
+import Corouse from '../components/Carousel/Corouse'
+
 
 const Home = () => {
-  const [genreMovie, setGenreMovie] = useState([])
-  const [initialLoading, setInitialLoading] = useState(true)
-
-  const apiKey = process.env.REACT_APP_API_KEY
-
-  const uploadMovie = async () => {
-    setInitialLoading(true)
-    await axios
-      .get(
-        `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=en-US`
-      )
-      .then((res) => {
-        if (res.status === 200) {
-          setGenreMovie(res.data.genres)
-          setInitialLoading(false)
-        }
-      })
-      .catch((e) => {
-        console.log(e.message)
-      })
-  }
-
-  useEffect(() => {
-    uploadMovie()
-  }, [])
-
   return (
     <div>
-      {!initialLoading ? (
-        <div>
-          <Banner />
-          <Search />
-          <Trending title='Trending' id='1' />
-          {genreMovie &&
-            genreMovie.map((item) => {
-              return (
-                <>
-                  <Trending title={item.name} id={item.id} key={item.id} />
-                </>
-              )
-            })}
-        </div>
-      ) : (
-        <div className='flex justify-center my-8'>
-          <Oval
-            height='50'
-            width='50'
-            color='grey'
-            secondaryColor='grey'
-            ariaLabel='loading'
-          />
-        </div>
-      )}
+      
+        <Corouse/>
     </div>
   )
 }
