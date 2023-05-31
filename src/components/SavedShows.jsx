4bd479bccc5679bf50e1c541203004ce810f4516
Shravan <ishravan919@gmail.com>
2023-05-31 23:40:44 +0530

    final changes made

--- src/components/SavedShows.jsx
+++ src/components/SavedShows.jsx
@@ -46,35 +46,55 @@ const SavedShows = () => {
           className='bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block'
           size={40}
         />
-        <div
-          ref={sliderRef}
-          className='w-full h-full overflow-x-hidden whitespace-nowrap relative'
-        >
-          {movies.map((item) => (
+        {movies.length === 0 ? (
+         <div className="w-full h-full flex flex-col justify-center items-center">
+         <p className="text-white text-4xl font-bold mb-4">
+           Oops! You don't have any favourite movies.
+         </p>
+         <p className="text-gray-300 text-lg mb-8">
+           Start exploring and saving your favourite movies now!
+         </p>
+         <Link to='/'>
+         <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+           Browse Movies
+         </button>
+         </Link>
+       </div>
+       
+       
+        ) : (
+          <div
+            ref={sliderRef}
+            className='w-full h-full overflow-x-hidden whitespace-nowrap relative'
+          >
             
-            <div
-              key={item.id}
-              className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2'
-            >
-              <img
-                className='w-full h-auto block'
-                src={`https://image.tmdb.org/t/p/w500/${item?.img}`}
-                alt={item?.title}
-              />
-              <div className='absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white'>
-              <Link to = {"/movie/" + item.title + "/" + item.id}>
-                <p className='white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center'>
-                  {item?.title}
-                </p>
-                </Link>
-                <p onClick={() => deleteShow(item.id)} className='absolute text-gray-300 top-4 right-4'>
-                  <AiOutlineClose />
-                </p>
+            {movies && movies.map((item) => (
+            
+              <div
+                key={item.id}
+                className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2'
+              >
+                  <Link to={'/movie/' + item.title + '/' + item.id}>
+                <img
+                  className='w-full h-auto block'
+                  src={`https://image.tmdb.org/t/p/w500/${item?.img}`}
+                  alt={item?.title}
+                />
+                <div className='absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white'>
+                  
+                    <p className='white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center'>
+                      {item?.title}
+                    </p>
+                    </div>
+                    </Link>
+                <p onClick={()=> deleteShow(item.id)} className='absolute text-gray-300 top-4 right-4'><AiOutlineClose /></p>
+                  
               </div>
-            </div>
-           
-          ))}
-        </div>
+             
+            ))}
+            
+          </div>
+        )}
         <MdChevronRight
           onClick={slideRight}
           className='bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block'
