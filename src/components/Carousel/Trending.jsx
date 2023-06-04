161e5cdba0a9daac63a398ac567c62e9da290358
Dunna Avinash <56786344+Avinash905@users.noreply.github.com>
2023-06-05 02:22:33 +0530

    Added key prop

--- src/components/Carousel/Trending.jsx
+++ src/components/Carousel/Trending.jsx
@@ -1,52 +1,52 @@
-import { Splide, SplideSlide } from '@splidejs/react-splide'
-import '@splidejs/react-splide/css'
-import axios from 'axios'
-import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import { Oval } from 'react-loader-spinner'
-import styles from '../../style'
-import MovieCard from '../Cards/MovieCard'
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Oval } from "react-loader-spinner";
+import styles from "../../style";
+import MovieCard from "../Cards/MovieCard";
 
 const Trending = (props) => {
-  const apiKey = process.env.REACT_APP_API_KEY
-  const [initialLoading, setinitalLoading] = useState(true)
-  const [Movies, setMovies] = useState([])
+  const apiKey = process.env.REACT_APP_API_KEY;
+  const [initialLoading, setinitalLoading] = useState(true);
+  const [Movies, setMovies] = useState([]);
 
   const upload = async (url) => {
-    setinitalLoading(true)
+    setinitalLoading(true);
     await axios
       .get(url)
       .then((res) => {
         if (res.status === 200) {
-          setMovies(res.data.results)
-          setinitalLoading(false)
+          setMovies(res.data.results);
+          setinitalLoading(false);
         }
       })
       .catch((e) => {
-        console.log(e.message)
-      })
-  }
+        console.log(e.message);
+      });
+  };
 
   useEffect(() => {
     let url =
-      props.title === 'Trending'
+      props.title === "Trending"
         ? `https://api.themoviedb.org/3/trending/movie/day?api_key=${apiKey}`
-        : `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=${props.id}`
-    upload(url)
-  }, [])
+        : `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=${props.id}`;
+    upload(url);
+  }, []);
 
   return (
     <>
-      {' '}
+      {" "}
       {!initialLoading ? (
         <div className={`${styles.boxWidth} my-8`}>
-          {' '}
-          <div className='flex justify-between items-center px-4'>
+          {" "}
+          <div className="flex justify-between items-center px-4">
             <h2 className={`${styles.heading3}`}>{props.title}</h2>
-            <p className=''>
+            <p className="">
               <Link
                 className={`${styles.paragraph} hover:text-white duration-200`}
-                to={'/category/movie/' + props.title + '/' + props.id}
+                to={"/category/movie/" + props.title + "/" + props.id}
               >
                 Show all
               </Link>
@@ -54,8 +54,8 @@ const Trending = (props) => {
           </div>
           <Splide
             options={{
-              type: 'loop',
-              perPage: '6',
+              type: "loop",
+              perPage: "6",
               pagination: false,
               breakpoints: {
                 400: {
@@ -75,35 +75,35 @@ const Trending = (props) => {
                 },
               },
             }}
-            aria-label='My Favorite Images'
-            className='justify-center'
+            aria-label="My Favorite Images"
+            className="justify-center"
           >
             {Movies.map((movie) => {
               return (
-                <SplideSlide>
-                  <MovieCard movie={movie} key={movie.id} />
+                <SplideSlide key={movie.id}>
+                  <MovieCard movie={movie} />
                 </SplideSlide>
-              )
+              );
             })}
-          </Splide>{' '}
+          </Splide>{" "}
         </div>
       ) : (
-        <div className='flex justify-center my-8'>
+        <div className="flex justify-center my-8">
           <Oval
-            height='50'
-            width='50'
-            color='grey'
-            secondaryColor='grey'
-            ariaLabel='loading'
+            height="50"
+            width="50"
+            color="grey"
+            secondaryColor="grey"
+            ariaLabel="loading"
           />
         </div>
       )}
     </>
-  )
-}
+  );
+};
 
 Trending.defaultProps = {
-  title: 'Trending',
-}
+  title: "Trending",
+};
 
-export default Trending
+export default Trending;
