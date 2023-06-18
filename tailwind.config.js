cb085fa32a11501f18baea2f6ba7b2c22f26eb11
Dunna Avinash <56786344+Avinash905@users.noreply.github.com>
2023-06-19 03:44:17 +0530

    added scroll to top

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,28 @@ module.exports = {
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
       },
+      keyframes:{
+        updown: {
+          '0%': {
+            transform: 'translateY(0px)'
+          },
+          '25%': {
+            transform: 'translateY(10px)'
+          },
+          '50%': {
+            transform: 'translateY(0px)'
+          },
+          '75%': {
+            transform: 'translateY(-10px)'
+          },
+          '100%': {
+            transform: 'translateY(0px)'
+          },
+        }
+      },
+      animation: {
+        'arrow-updown': 'updown 3s linear infinite',
+      },
     },
   },
   plugins: [
