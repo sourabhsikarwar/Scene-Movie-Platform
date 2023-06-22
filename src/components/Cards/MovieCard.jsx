ce9ab3bd56baf30e6a5ce40efaf8e72ab5d21e6f
Sourabh Sikarwar <85223699+sourabhsikarwar@users.noreply.github.com>
2023-06-22 13:02:03 +0530

    UseEffect included

--- src/components/Cards/MovieCard.jsx
+++ src/components/Cards/MovieCard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "../../style";
 import { Link } from "react-router-dom";
 import { FaHeart, FaRegHeart } from "react-icons/fa";
