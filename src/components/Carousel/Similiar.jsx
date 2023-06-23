9f01dcaa6f7e8ccc8887e06d6fb9c50a0706b7c0
Anamika Yadav <aanyayadav419@gmail.com>
2023-06-24 00:42:20 +0530

    light/dark mode fix

--- src/components/Carousel/Similiar.jsx
+++ src/components/Carousel/Similiar.jsx
@@ -45,7 +45,7 @@ const Similiar = (props) => {
           />
         </div>
       ) : (
-        <div className={`${styles.boxWidth} my-8`}>
+        <div className={`${styles.boxWidth} dark:bg-primary dark:text-dimWhite py-8`}>
           <div className='flex justify-between items-center px-4'>
             <h2 className={`${styles.heading3} text-gray-900 dark:text-white`}>Similiar</h2>
           </div>
--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -5,9 +5,9 @@ function Pagination(props) {
   return (
     <>
     <div className="flex justify-center m-8">
-        <button className='px-4 py-2 border-[1px] border-blue-300 border-r-0 text-white rounded-l-md' onClick={props.goBack}>Prev</button>
-        <button className='px-4 py-2 border-[1px] border-blue-300 text-white'>{props.page}</button>
-        <button className='px-4 py-2 border-[1px] border-blue-300 border-l-0 text-white rounded-r-md' onClick={props.goNext}>Next</button>
+        <button className='px-4 py-2 border-[1px] border-blue-300 border-r-0 text-gray-900 dark:text-white rounded-l-md' onClick={props.goBack}>Prev</button>
+        <button className='px-4 py-2 border-[1px] border-blue-300 text-gray-900 dark:text-white'>{props.page}</button>
+        <button className='px-4 py-2 border-[1px] border-blue-300 border-l-0 text-gray-900 dark:text-white rounded-r-md' onClick={props.goNext}>Next</button>
     </div>
     </>
   )
