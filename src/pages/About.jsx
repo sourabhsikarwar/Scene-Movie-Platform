385a1558e6f7bf6acb00d14fcaaf1fce39caa265
rajendra mandliya <119125519+mandliyarajendra11@users.noreply.github.com>
2023-06-24 18:08:29 +0530

    warning removed

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,7 +6,7 @@ import AdminAbout from "../components/AdminAbout";
 import background from '../assets/image/bg1.png'
 const About = () => {
   return (
-    <div>
+    <div className="dark:bg-primary dark:text-dimWhite p-1 ">
     <section
         className="section-bg text-gray-600 body-font overflow-hidden bg-[#656565cf] bg-blend-multiply"
         style={{
@@ -23,16 +23,16 @@ const About = () => {
             </div>
             </section>
 
-        <p className=" my-20  text-2xl leading-10  text-center">
+        <div className=" my-20  text-2xl leading-10  text-center">
             <strong >Welcome to</strong> <br/>
-           <span className={`${styles.heading2} text-black mb-3 `} > scene movie platform </span><br/>
+           <span className={`${styles.heading2} text-black mb-3 dark:text-white `} > scene movie platform </span><br/>
              we're passionate about bringing you the best entertainment experience right at your
             fingertips.<br/>
              Our goal is to provide you with a platform that offers a 
             vast collection of movies, TV shows, and documentaries .
-            <p className="mb-5">We welcome contributions from developers like you to help make our project even better</p>
-          <Button content="contribute"/>
-          </p>
+            <pre className="mb-5">We welcome contributions from developers like you to help make our project even better</pre>
+          <a href="https://github.com/sourabhsikarwar/Scene-Movie-Platform" target="_blank"><Button content="contribute" /></a>
+          </div>
     <AdminAbout/>
     <Contributor/>
     </div>
