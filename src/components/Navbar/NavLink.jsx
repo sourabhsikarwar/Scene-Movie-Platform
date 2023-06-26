afbf080cb15460b32a88b185c0903a39feb321fc
Anamika Yadav <aanyayadav419@gmail.com>
2023-06-27 02:56:10 +0530

    movies display in mobile view

--- src/components/Navbar/NavLink.jsx
+++ src/components/Navbar/NavLink.jsx
@@ -1,22 +1,22 @@
-import React, { useState } from 'react'
-import { Link, useLocation } from 'react-router-dom'
-import { Links } from './Links'
+import React, { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { Links } from "./Links";
 
 const NavLinks = ({ onMovieLinkClick }) => {
-  const [heading, setHeading] = useState('')
-  const location = useLocation()
-  
+  const [heading, setHeading] = useState("");
+  const location = useLocation();
+
   return (
     <>
       {Links.map((link) => (
         <div key={link.name}>
-          <div className='mx-3 text-left md:cursor-pointer group'>
+          <div className="mx-3 text-left md:cursor-pointer group">
             <h1
               className={`${
                 location.pathname.startsWith("/category/movie") ? "active" : ""
               } my-7 flex navDropdown justify-between mx-2 md:ml-0 items-center md:mr-0 mr-5 group`}
               onClick={() => {
-                heading !== link.name ? setHeading(link.name) : setHeading('')
+                heading !== link.name ? setHeading(link.name) : setHeading("");
               }}
             >
               {link.name}
@@ -36,23 +36,23 @@ const NavLinks = ({ onMovieLinkClick }) => {
                 <div className="absolute top-[60px] hidden group-hover:md:block hover:md:block z-50">
                   <div className="bg-gray-300 dark:text-dimWhite dark:bg-secondary p-8 grid grid-cols-3 gap-x-10 rounded-lg">
                     <ul>
-                    {link.genres.map((sLink) => (
-                      <li
-                        key={sLink.id}
-                        className="text-sm text-gray-900 dark:text-gray-300 font-light my-2.5"
-                      >
-                        <Link
-                          to={
-                            link.name === "Movies"
-                              ? `/category/movie/${sLink.name}/${sLink.id}`
-                              : `/category/movie/${sLink.name}/${sLink.id}`
-                          }
-                          className="hover:underline dark:hover:text-white"
+                      {link.genres.map((sLink) => (
+                        <li
+                          key={sLink.id}
+                          className="text-sm text-gray-900 dark:text-gray-300 font-light my-2.5"
                         >
-                          {sLink.name}
-                        </Link>
-                      </li>
-                    ))}
+                          <Link
+                            to={
+                              link.name === "Movies"
+                                ? `/category/movie/${sLink.name}/${sLink.id}`
+                                : `/category/movie/${sLink.name}/${sLink.id}`
+                            }
+                            className="hover:underline dark:hover:text-white"
+                          >
+                            {sLink.name}
+                          </Link>
+                        </li>
+                      ))}
                     </ul>
                   </div>
                 </div>
@@ -69,14 +69,20 @@ const NavLinks = ({ onMovieLinkClick }) => {
           >
             <div className="grid grid-cols-2">
               <ul>
-              {link.genres.map((sLinks) => (
-                <li
-                  className="py-3 pl-7 font-light"
-                  key={sLinks.id}
-                >
-                  <Link to={sLinks.id} onClick={onMovieLinkClick} >{sLinks.name}</Link>
-                </li>
-              ))}
+                {link.genres.map((sLinks) => (
+                  <li className="py-3 pl-7 font-light" key={sLinks.id}>
+                    <Link
+                      to={
+                        link.name === "Movies"
+                          ? `/category/movie/${sLinks.name}/${sLinks.id}`
+                          : `/category/movie/${sLinks.name}/${sLinks.id}`
+                      }
+                      onClick={onMovieLinkClick}
+                    >
+                      {sLinks.name}
+                    </Link>
+                  </li>
+                ))}
               </ul>
             </div>
           </div>
