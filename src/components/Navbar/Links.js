c1525a0110786113071e238cc1768a1e57f8a8ed
Anamika Yadav <aanyayadav419@gmail.com>
2023-07-02 16:57:22 +0530

    added tv genres

--- src/components/Navbar/Links.js
+++ src/components/Navbar/Links.js
@@ -83,37 +83,69 @@ export const TVShowsLinks = [
     subMenu: true,
     genres: [
       {
-        id: 80,
-        name: "Crime",
+        "id": 10759,
+        "name": "Action & Adventure"
       },
       {
-        id: 18,
-        name: "Drama",
+        "id": 16,
+        "name": "Animation"
       },
       {
-        id: 10751,
-        name: "Family",
+        "id": 35,
+        "name": "Komödie"
       },
       {
-        id: 10765,
-        name: "Sci-Fi & Fantasy",
+        "id": 80,
+        "name": "Krimi"
       },
       {
-        id: 9648,
-        name: "Mystery",
+        "id": 99,
+        "name": "Dokumentarfilm"
       },
       {
-        id: 10749,
-        name: "Romance",
+        "id": 18,
+        "name": "Drama"
       },
       {
-        id: 10759,
-        name: "Action & Adventure",
+        "id": 10751,
+        "name": "Familie"
       },
       {
-        id: 35,
-        name: "Comedy",
+        "id": 10762,
+        "name": "Kids"
       },
+      {
+        "id": 9648,
+        "name": "Mystery"
+      },
+      {
+        "id": 10763,
+        "name": "News"
+      },
+      {
+        "id": 10764,
+        "name": "Reality"
+      },
+      {
+        "id": 10765,
+        "name": "Sci-Fi & Fantasy"
+      },
+      {
+        "id": 10766,
+        "name": "Soap"
+      },
+      {
+        "id": 10767,
+        "name": "Talk"
+      },
+      {
+        "id": 10768,
+        "name": "War & Politics"
+      },
+      {
+        "id": 37,
+        "name": "Western"
+      }
     ],
   },
 ];
