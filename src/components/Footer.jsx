6b3f7490b1b8d37a927b22d2223d497421508175
Anil <anilkoduru27@gmail.com>
2023-07-07 22:39:16 +0530

    Footer Enhancement

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,55 +10,55 @@ const Footer = () => {
   
   return (
     <footer className='bg-gray-200 text-gray-900 dark:bg-primary dark:text-dimWhite body-font'>
-    <div className="w-80vw mx-auto max-w-1200px"style={{ marginTop: "-32px" }}>
-      <div className="flex justify-around flex-wr mtap">
-      <div className="mb-6 mt-8">
-        <div className="font-bold text-lg mb-3 text-black text-opacity-90 dark:text-zinc-50">Quick Links</div>
-        <ul className="list-none m-0 p-0">
-               <li className="leading-8 text-base text-black text-opacity-80 dark:text-zinc-50">
+    <div className="w-80vw mx-auto max-w-1200px"style={{ marginTop: "-12px"}}>
+      <div className="flex justify-around flex-wr mtap" style={{display:"flex",flexDirection:"column"}}>
+      <div className="mb-4 mt-10 ml-auto mr-auto">
+           {/* <div className="font-bold text-lg mb-3 text-black text-opacity-90 dark:text-zinc-50">Community</div> */}
+           <ul style={{display:"flex"}}>
+                <li className="leading-8 text-base text-black text-opacity-80 dark:text-zinc-50 ml-3 mr-3" style={{fontSize:"30px"}}>
+                <a href="https://github.com/sourabhsikarwar/Scene-Movie-Platform" aria-label="Follow me on Github" target="_blank" className={hoverEffect}><AiFillGithub className="mr-1" /></a>
+                </li>
+                <li className="leading-8 text-base text-black text-opacity-80 dark:text-zinc-50 ml-3 mr-3" style={{fontSize:"30px"}}>
+                   <a href="#" aria-label="Follow me on Twitter"  className={hoverEffect}><AiFillTwitterCircle className="mr-1 transition ease duration-200 group-hover:text-sky-500" /></a>
+                </li>
+                <li className="leading-8 text-base text-black text-opacity-80 dark:text-zinc-50 ml-3 mr-3" style={{fontSize:"30px"}}>
+                   <a href="https://www.linkedin.com/in/sourabhsikarwar/" target="_blank" aria-label="Follow me on Linkedin" className={hoverEffect}><AiFillLinkedin className="mr-1 transition ease duration-200 group-hover:text-sky-600" /></a>
+                </li>
+                <li className="leading-8 text-base text-black text-opacity-80 dark:text-zinc-50 ml-3 mr-3" style={{fontSize:"30px"}}>
+                   <a href="#" aria-label="Follow me on Instagram" className={hoverEffect}><AiFillInstagram className="mr-1 transition ease duration-200 group-hover:text-rose-500" /></a>
+                </li>
+                <li className="leading-8 text-base text-black text-opacity-80 dark:text-zinc-50 ml-3 mr-3" style={{fontSize:"30px"}}>
+                   <a href="#" aria-label="Follow me on Facebook" className={hoverEffect}><AiFillFacebook className="mr-1 transition ease duration-200 group-hover:text-blue-700" /></a>
+                </li>
+             </ul>
+        </div>
+      <div className="mb-4 mt-4 ml-auto mr-auto">
+        {/* <div className="font-bold text-lg mb-3 text-black text-opacity-90 dark:text-zinc-50">Quick Links</div> */}
+        <ul className="list-none m-0 p-0" style={{display: "flex",flexWrap: "wrap"}}>
+               <li className="leading-8 text-base text-black text-opacity-80 dark:text-zinc-50 ml-6 mr-6">
                 <Link to="/" className={hoverEffect}>Home</Link>
                 </li>
-                <li className="leading-8 text-base text-black text-opacity-80 dark:text-zinc-50">
+                <li className="leading-8 text-base text-black text-opacity-80 dark:text-zinc-50 ml-6 mr-6">
                 <Link to="/about" className={hoverEffect}>About</Link>
                 </li>
-                <li className="leading-8 text-base text-black text-opacity-80 dark:text-zinc-50">
+                <li className="leading-8 text-base text-black text-opacity-80 dark:text-zinc-50 ml-6 mr-6">
                 <Link to="/recommend" className={hoverEffect}>For You</Link>
                 </li>
-                <li className="leading-8 text-base text-black text-opacity-80 dark:text-zinc-50">
+                <li className="leading-8 text-base text-black text-opacity-80 dark:text-zinc-50 ml-6 mr-6">
                 <Link to="/profile" className={hoverEffect}>Profile</Link>
                 </li>
-                <li className="leading-8 text-base text-black text-opacity-80 dark:text-zinc-50">
+                <li className="leading-8 text-base text-black text-opacity-80 dark:text-zinc-50 ml-6 mr-6">
                 <Link to="/favourite" className={hoverEffect}>TV Shows</Link>
                 </li>
         </ul>
         </div>
-      <div className="mb-24 mt-8">
-           <div className="font-bold text-lg mb-3 text-black text-opacity-90 dark:text-zinc-50">Community</div>
-           <ul>
-                <li className="leading-8 text-base text-black text-opacity-80 dark:text-zinc-50">
-                <a href="https://github.com/sourabhsikarwar/Scene-Movie-Platform" aria-label="Follow me on Github" target="_blank" className={hoverEffect}><AiFillGithub className="mr-1" />Github</a>
-                </li>
-                <li className="leading-8 text-base text-black text-opacity-80 dark:text-zinc-50">
-                   <a href="#" aria-label="Follow me on Twitter"  className={hoverEffect}><AiFillTwitterCircle className="mr-1 transition ease duration-200 group-hover:text-sky-500" />Twitter</a>
-                </li>
-                <li className="leading-8 text-base text-black text-opacity-80 dark:text-zinc-50">
-                   <a href="https://www.linkedin.com/in/sourabhsikarwar/" target="_blank" aria-label="Follow me on Linkedin" className={hoverEffect}><AiFillLinkedin className="mr-1 transition ease duration-200 group-hover:text-sky-600" />Linkedin</a>
-                </li>
-                <li className="leading-8 text-base text-black text-opacity-80 dark:text-zinc-50">
-                   <a href="#" aria-label="Follow me on Instagram" className={hoverEffect}><AiFillInstagram className="mr-1 transition ease duration-200 group-hover:text-rose-500" />Instagram</a>
-                </li>
-                <li className="leading-8 text-base text-black text-opacity-80 dark:text-zinc-50">
-                   <a href="#" aria-label="Follow me on Facebook" className={hoverEffect}><AiFillFacebook className="mr-1 transition ease duration-200 group-hover:text-blue-700" />Facebook</a>
-                </li>
-             </ul>
-        </div>
-      <div className="mb-24 mt-8">
-           <div className="font-bold text-lg mb-3 text-black text-opacity-90 dark:text-zinc-50">Need Help?  </div>
-           <ul>
-                <li className="leading-8 text-base text-black text-opacity-80 dark:text-zinc-50">
+      <div className="mb-10 mt-4 ml-auto mr-auto">
+           {/* <div className="font-bold text-lg mb-3 text-black text-opacity-90 dark:text-zinc-50">Need Help?  </div> */}
+           <ul style={{display:"flex"}}>
+                <li className="leading-8 text-base text-black text-opacity-80 dark:text-zinc-50 ml-6 mr-6">
                    <a href="#" className={hoverEffect}>Visit Help Center</a>
                 </li>
-                <li className="leading-8 text-base text-black text-opacity-80 dark:text-zinc-50">
+                <li className="leading-8 text-base text-black text-opacity-80 dark:text-zinc-50 ml-6 mr-6">
                    <a href="#"className={hoverEffect}>Share Feedback</a>
                 </li>
              </ul>
