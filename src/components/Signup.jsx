3a7f63ecc52666a184875d80815e4b745a81f8bf
Your Name <you@example.com>
2023-07-09 15:08:04 +0600

    Deploy successfully [BUG]: Sign up Form & Sign up button is not responsive #342

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,5 +1,4 @@
 
-import React, { useState } from "react";
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import bg from "../assets/image/bg2.webp";
