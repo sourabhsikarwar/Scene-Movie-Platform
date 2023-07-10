e48e3f24c0eb4bd90a81a191e3efa77a78100acf
Anamika Yadav <aanyayadav419@gmail.com>
2023-07-11 00:33:14 +0530

    banner ui changed

--- src/components/Banner/TvBanner.jsx
+++ src/components/Banner/TvBanner.jsx
@@ -1,24 +1,19 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
-import Youtube from "react-youtube";
 import { Oval } from "react-loader-spinner";
+import styles from "../../style";
 // for star rating convert number into star
 import Star from "../SingleMovieCast/Star";
 //  format price is used to format country currency
-import FormatPrice from "../SingleMovieCast/FormatPrice";
-import "../SingleMovieCast/style.css";
 import { useParams } from "react-router-dom";
 
 const MovieBanner = (props) => {
   const MOVIE_API = "https://api.themoviedb.org/3";
   const { tvId, title } = useParams();
-  const [playing, setPlaying] = useState(false);
   const [Tv, setTv] = useState({});
 
-  const [trailer, setTrailer] = useState(null);
   const [initialLoading, setInitialLoading] = useState(true);
   const apiKey = process.env.REACT_APP_API_KEY;
-  const totalMinutes = Tv.runtime;
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -35,119 +30,76 @@ const MovieBanner = (props) => {
         setInitialLoading(false);
       });
   };
-  const handleTrailer = async () => {
-    setInitialLoading(true);
-    const { data } = await axios.get(`${MOVIE_API}/tv/${props?.id}`, {
-      params: {
-        api_key: apiKey,
-        append_to_response: "videos",
-      },
-    });
-
-    if (data.videos && data.videos.results) {
-      const trailer = data.videos.results.find(
-        (vid) => vid.name === "Official Trailer"
-      );
-      setTrailer(trailer ? trailer : data.videos.results[0]);
-      setPlaying(true);
-    }
-
-    // setMovie(data)
-    setInitialLoading(false);
-  };
 
   return (
     <>
       {!initialLoading ? (
-        <section className="container movie-container" key={Tv.id}>
-          <div className="grid grid-two-column">
-            <div className="product_image">
-              <img
-                width={"60%"}
-                className=" object-cover object-center rounded  pt-10"
-                src={`https://image.tmdb.org/t/p/original/${Tv.poster_path}`}
-                alt={Tv.title}
-              />
-            </div>
-            <div className="product-data">
-              <h2>{Tv.name}</h2>
-              <Star stars={Tv.vote_average / 2} reviews={Tv.vote_count} />
-              <p className="product-data-price product-data-real-price">
-                Release :{" "}
-                {Tv.first_air_date.toString().split("-").reverse().join("-")}
-              </p>
-              <div style={{ display: "flex", justifyContent: "space-between" }}>
-                <p style={{ marginRight: "10px" }}>
-                  Total Seasons: &nbsp;
-                  <span style={{ fontWeight: "bold" }}>
-                    {Tv.number_of_seasons}
-                  </span>
-                </p>
-                <p>
-                  Total Episodes: &nbsp;
-                  <span style={{ fontWeight: "bold" }}>
-                    {Tv.number_of_episodes}
-                  </span>
-                </p>
-                {/* <p>Episode Duration &nbsp;
-                    <span style={{fontWeight: "bold"}}>{Math.floor(Tv.episode_run_time / 60)} h&nbsp; {Math.floor(Tv.episode_run_time % 60)}</span>
-                  </p> */}
-              </div>
-              <p>{Tv.overview}</p>
-              <div className="product-data-info">
-                <p>
-                  Languages :
-                  {Tv.spoken_languages.map((ele) => (
-                    <span>{ele.english_name} </span>
-                  ))}
-                </p>
-                <p>
-                  Genres :{" "}
-                  {Tv.genres.map((ele) => (
-                    <span>{ele.name} </span>
-                  ))}
-                </p>
-              </div>
-              <hr />
-              {playing ? (
-                <div className="mx-auto">
-                  <Youtube
-                    videoId={trailer.key}
-                    className={"youtube amru"}
-                    containerClassName={"youtube-container amru"}
-                    opts={{
-                      playerVars: {
-                        autoplay: 1,
-                        controls: 0,
-                        cc_load_policy: 0,
-                        fs: 0,
-                        iv_load_policy: 0,
-                        modestbranding: 0,
-                        rel: 0,
-                        showinfo: 0,
-                      },
-                    }}
-                  />
-                  <button
-                    onClick={() => setPlaying(false)}
-                    className={"button close-video"}
-                  >
-                    Close
-                  </button>
+        <section
+          className="relative text-gray-600 body-font overflow-hidden bg-[#656565cf] bg-blend-multiply"
+          style={{
+            backgroundImage: `url(https://image.tmdb.org/t/p/original/${
+              Tv.backdrop_path ? Tv.backdrop_path : Tv.poster_path
+            })`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+          key={Tv.id}
+        >
+          <div className="absolute inset-0 bg-black opacity-40"></div>
+          <div
+            className={`${styles.boxWidth} px-4 py-8 mx-auto lg:h-screen h-full flex items-center relative z-10`}
+          >
+            <div className="mx-auto flex flex-wrap flex-row">
+              <div className="m-auto w-2/3 h-full sm:w-2/4 md:w-1/4 my-[1%] bg-gray-200 rounded shadow-md">
+                <img
+                  alt={`${Tv.poster_path}`}
+                  className="w-full h-full object-cover object-center rounded"
+                  src={`https://image.tmdb.org/t/p/original/${Tv.poster_path}`}
+                  loading="lazy"
+                />
+              </div>{" "}
+              <div className="py-3 lg:py-4 flex flex-col items-center md:items-start md:w-2/3 mx-auto">
+                <h1
+                  className={`${styles.heading2} font-extrabold text-center md:text-left`}
+                >
+                  {Tv.name}
+                </h1>
+                <Star stars={Tv.vote_average / 2} reviews={Tv.vote_count} />
+                <div className="flex flex-row justify-between items-start text-gray-300 mt-3">
+                  {Tv.first_air_date.split("-")[0]}
+                  <span className="mx-2">|</span>
+                  {Tv.spoken_languages[0].english_name}
+                  <span className="mx-2">|</span>
+                  {Tv.genres[0].name}
                 </div>
-              ) : (
-                <div className="flex my-4">
-                  <button
-                    // onClick={handleTrailer}
-                    className="flex bg-blue-gradient text-black border-0 py-2 px-4 focus:outline-none hover:bg-indigo-600 rounded"
+                <div
+                  className="my-3"
+                  style={{ display: "flex", justifyContent: "space-between" }}
+                >
+                  <p
+                    className={`${styles.paragraph} leading-4 sm:text-sm lg:text-base text-center md:text-left`}
+                    style={{ marginRight: "10px" }}
                   >
-                    Watch
-                  </button>
-                  <button className="rounded-full w-10 h-10 bg-white hover:bg-gray-100 duration-200 p-0 border-0 inline-flex items-center justify-center text-red-500 ml-4">
-                    <ion-icon name="heart"></ion-icon>
-                  </button>
+                    Total Seasons: &nbsp;
+                    <span style={{ fontWeight: "bold" }}>
+                      {Tv.number_of_seasons}
+                    </span>
+                  </p>
+                  <p
+                    className={`${styles.paragraph} leading-4 sm:text-sm lg:text-base text-center md:text-left`}
+                  >
+                    Total Episodes: &nbsp;
+                    <span style={{ fontWeight: "bold" }}>
+                      {Tv.number_of_episodes}
+                    </span>
+                  </p>
                 </div>
-              )}
+                <p
+                  className={`${styles.paragraph} leading-4 sm:text-sm lg:text-base text-center md:text-left`}
+                >
+                  {Tv.overview}
+                </p>
+              </div>
             </div>
           </div>
         </section>
--- src/components/SingleMovieCast/Star.jsx
+++ src/components/SingleMovieCast/Star.jsx
@@ -1,22 +1,32 @@
-import {FaStar,FaStarHalfAlt}from 'react-icons/fa'
-import {AiOutlineStar} from 'react-icons/ai'
-import './style.css'
-const Star = ({stars,reviews}) => {
-  const ratingStar=Array.from({length:5},(ele,index)=>{
-    let number=index+0.5
+import { FaStar, FaStarHalfAlt } from "react-icons/fa";
+import { AiOutlineStar } from "react-icons/ai";
+import "./style.css";
+import styles from "../../style";
+
+const Star = ({ stars, reviews }) => {
+  const ratingStar = Array.from({ length: 5 }, (ele, index) => {
+    let number = index + 0.5;
     return (
-        <span key={index}>
-            {
-                stars>=index+1?<FaStar className='icon'/>:stars>=number?<FaStarHalfAlt  className='icon'/>:<AiOutlineStar  className='icon'/>
-            }
-        </span>
-    )
-  })
-    return (
-            <div className='icon-style'>
-    {ratingStar}
-    <p>{reviews} Total reviews</p>
+      <span key={index}>
+        {stars >= index + 1 ? (
+          <FaStar className="icon" />
+        ) : stars >= number ? (
+          <FaStarHalfAlt className="icon" />
+        ) : (
+          <AiOutlineStar className="icon" />
+        )}
+      </span>
+    );
+  });
+  return (
+    <div className="icon-style">
+      {ratingStar}
+      <p
+        className={`${styles.paragraph} mb-2 leading-4 sm:text-sm lg:text-base text-center md:text-left`}
+      >
+        {reviews} Total reviews
+      </p>
     </div>
-  )
-}
-export default Star
+  );
+};
+export default Star;
