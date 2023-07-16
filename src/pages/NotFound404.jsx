cec0e002bdb7a0f8f9ff827e5d8e245b8ce236ff
Shreya Dhiman <shreyanksha2004@gmail.com>
2023-07-16 23:55:34 +0530

    404 error page improved

--- src/pages/NotFound404.jsx
+++ src/pages/NotFound404.jsx
@@ -4,12 +4,12 @@ import Notfound from "../assets/image/notfound.webp";
 const NotFound404 = () => {
   return (
     <div className="error-container bg-gray-200 dark:bg-primary">
-      <img className="ops" src={Notfound} alt="404 Not Found" loading='lazy'/>
-      <br />
-      <h3 className="text-sky-400 dark:text-gradient font-poppins font-medium md:text-[24px] text-[24px] leading-relaxed mb-4">
+      <div className="error-text">
+        <h1>Error Page 404 </h1>
+      <h2 className="text-sky-400 dark:text-gradient font-poppins font-medium md:text-[24px] text-[24px] leading-relaxed mb-4">
         Opps! Page not found
         <br /> Sorry, the page you're looking for doesn't exist.
-      </h3>
+      </h2>
       <br />
 
       <Link
@@ -18,6 +18,8 @@ const NotFound404 = () => {
       >
         Back to Home
       </Link>
+      </div>
+      <img className="ops" src={Notfound} alt="404 Not Found" loading='lazy'/>
     </div>
   );
 };
