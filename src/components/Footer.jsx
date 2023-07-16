e369b1a71e0144d3a75df9c0ba792e26be42a5b4
Avinash <avinash.90527@gmail.com>
2023-07-17 01:57:40 +0530

    added accessibility to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,6 +13,7 @@ const Footer = () => {
                 <img
                   src={logo}
                   alt="LOGO"
+                  aria-label="footer logo"
                 />
               </div>
               <div className="content-box">
@@ -20,19 +21,19 @@ const Footer = () => {
                   <h4>Quick Links</h4>
                   <ul>
                     <li>
-                      <a href="/">Home</a>
+                      <a href="/" aria-label="Go to home page">Home</a>
                     </li>
                     <li>
-                      <a href="/about">About</a>
+                      <a href="/about" aria-label="Go to about page">About</a>
                     </li>
                     <li>
-                      <a href="/recommend">For You</a>
+                      <a href="/recommend" aria-label="Go to recommendation page">For You</a>
                     </li>
                     <li>
-                      <a href="/profile">Profile</a>
+                      <a href="/profile" aria-label="Go to your profile page">Profile</a>
                     </li>
                     <li>
-                      <a href="/favourite">TV Shows</a>
+                      <a href="/favourite" aria-label="Go to your favorites page">TV Shows</a>
                     </li>
                   </ul>
                 </div>
@@ -50,19 +51,34 @@ const Footer = () => {
                 <div className="footer-col">
                   <h4>Community</h4>
                   <div className="social-links">
-                    <a href="#">
+                    <a href="#" aria-label="Follow me on Facebook"
+                      title="Facebook (External Link)"
+                      rel="noopener noreferrer"
+                      target="_blank">
                       <i className="fab fa-facebook-f"></i>
                     </a>
-                    <a href="#">
+                    <a href="#" aria-label="Follow me on Twitter"
+                      title="Twitter (External Link)"
+                      rel="noopener noreferrer"
+                      target="_blank">
                       <i className="fab fa-twitter"></i>
                     </a>
-                    <a href="#">
+                    <a href="#" aria-label="Follow me on Instagram"
+                      title="Instagram (External Link)"
+                      rel="noopener noreferrer"
+                      target="_blank">
                       <i className="fab fa-instagram"></i>
                     </a>
-                    <a href="https://www.linkedin.com/in/sourabhsikarwar/">
+                    <a href="https://www.linkedin.com/in/sourabhsikarwar/" aria-label="Follow me on Linkedin"
+                      title="Linkedin (External Link)"
+                      rel="noopener noreferrer"
+                      target="_blank">
                       <i className="fab fa-linkedin-in"></i>
                     </a>
-                    <a href="https://github.com/sourabhsikarwar/Scene-Movie-Platform">
+                    <a href="https://github.com/sourabhsikarwar/Scene-Movie-Platform" aria-label="Follow me on Github"
+                      title="Github (External Link)"
+                      rel="noopener noreferrer"
+                      target="_blank">
                       <i className="fab fa-github"></i>
                     </a>
                   </div>
