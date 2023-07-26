32ff09e3f1fab3414f7bbda4fccb7021290b6ad2
senthil-k8s <senthilathiban0@gmail.com>
2023-07-26 20:05:12 +0530

    feat: Profile Page ReDesigned

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,11 +1,13 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { useUserAuth } from "../context/authContext";
 import avatar from "../assets/image/avatar.webp"
 import show from "../assets/image/show.webp";
 import hide from "../assets/image/hide.webp";
 import { toast } from 'react-toastify';
 import styles from "../style";
-import  { FaUserLock } from 'react-icons/fa';
+import { FaUserLock } from 'react-icons/fa';
+import bg from "../assets/image/bg2.webp";
+import { Link } from "react-router-dom";
 
 const Profile = () => {
   const { user, profileUpdate } = useUserAuth();
@@ -14,6 +16,7 @@ const Profile = () => {
     username: user?.displayName,
     email: user?.email,
     image: user?.photoURL,
+    contact: user?.contact,
     password: '',
   });
   const [errors, setErrors] = useState({});
@@ -67,7 +70,7 @@ const Profile = () => {
           success: 'Profile update successful',
           error: 'Error updating profile'
         }
-    );
+      );
     } catch (err) {
       console.log(err)
       setError("Error updating profile");
@@ -88,109 +91,136 @@ const Profile = () => {
   };
 
   return (
-    <section className="bg-gray-100 text-gray-900 dark:bg-primary dark:text-white body-font justify-center flex items-center h-full">
-      {/* <div className='flex items-center lg:w-3/5 mx-auto pb-10 mb-10 flex-col'>
-        <div className='sm:w-36 sm:h-36 h-24 w-24 mx-auto my-4 md:my-8 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0 overflow-hidden'>
-          <img src={avatar} alt='' />
-        </div>
-        <div className='flex flex-col text-center gap-4'>
-          <h2 className='text-white text-2xl md:text-4xl title-font font-bold mb-2'>
-            {userData.name}
-          </h2>
-          <p className='leading-relaxed text-base text-dimWhite'>
-            Email: {userData.email}
-          </p>
-          <p className='leading-relaxed text-base text-dimWhite'>
-            Contact no.: {userData.contact}
-          </p>
-          <p className='leading-relaxed text-base text-dimWhite'>
-            Date of Birth: {userData.Dob}
-          </p>
-        </div>
-      </div> */}
-      <div className="bg-gray-100 dark:bg-primary sm:dark:bg-secondary w-fit pt-5 px-0 md:px-[100px] md:mx-0 mx-10 rounded-lg mt-[30px] mb-[34px] flex md:flex-row flex-col gap-10 items-center">
-        <div className="bg-gray-300 text-gray-900 dark:bg-primary rounded-lg p-8 flex flex-col md:mx-auto w-full my-16 relative">
-          {/* Profile photo */}
-        <div className='h-1/3 sm:h-28 w-1/3 sm:w-28 mx-auto sm:my-4 md:my-8 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0 overflow-hidden sm:absolute -top-20 left-28'>
-          <img src={data.image ? data.image : avatar} alt='' className="h-28 w-28 rounded-full" />
-        </div>
-          <h2 className={`text-center text-sky-400 dark:text-gradient ${styles.heading3} mb-4 pt-8 sm:pt-12 mx-auto`}>Update Profile</h2>
-          {error && <p className="text-red-600">{`${error}`}<FaUserLock style={{display:"inline-block", marginLeft:"10px"}} /></p>}
-          <div className="relative mb-4 w-full">
-            <label htmlFor="username" className="leading-8 text-sm text-gray-900 dark:text-white">
-              Username
-            </label>
-            <input
-              value={data.username}
-              type="text"
-              id="username"
-              name="username"
-              placeholder="Enter username"
-              className={`w-full bg-white rounded border ${
-                errors.username ? "border-red-500" : "border-gray-300"
-              } focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-900 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out z-0`}
-              onChange={handleInputs}
-              onKeyDown={handleKeyDown}
-              autoComplete="off"
-            />
-            {errors.username && <p className="text-red-500">{errors.username}</p>}
-          </div>
-          <div className="relative mb-4">
-            <label htmlFor="email" className="leading-8 text-sm text-gray-900 dark:text-white">
-              Email
-            </label>
-            <input
-              value={data.email}
-              type="email"
-              id="email"
-              name="email"
-              placeholder="xyz@gmail.com"
-              className={`w-full bg-white rounded border ${
-                errors.email ? "border-red-500" : "border-gray-300"
-              } focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-900 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out z-0`}
-              onChange={handleInputs}
-              onKeyDown={handleKeyDown}
-              autoComplete="off"
-            />
-            {errors.email && <p className="text-red-500">{errors.email}</p>}
-          </div>
-          <div className="relative mb-4">
-            <label htmlFor="password" className="leading-8 text-sm text-gray-900 dark:text-white">
-              Password
-            </label>
-            <input
-              value={data.password}
-              type={passwordType}
-              id="password"
-              name="password"
-              placeholder="Enter your password"
-              className={`w-full bg-white rounded border ${
-                errors.password ? "border-red-500" : "border-gray-300"
-              } focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out z-0`}
-              onChange={handleInputs}
-              onKeyDown={handleKeyDown}
-            />
-            <button
-              onClick={passwordToggle}
-              className="absolute inset-y-0 right-0 top-7 pr-3 flex items-center pointer-events-cursor-pointer"
-            >
-              <img
-                height={30}
-                width={30}
-                src={passwordType === "password" ? hide : show}
-                alt="Toggle password visibility" loading='lazy'
-              />
-            </button>
-            {errors.password && (
-              <p className="text-red-500">{errors.password}</p>
-            )}
-          </div>
-          <button
-            className="text-black bg-blue-gradient mt-2 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
-            onClick={handleSubmit}
-          >
-            Update
+    <section
+      className="bg-gray-100  text-gray-900 dark:bg-primary dark:text-white body-font justify-center flex items-center h-full"
+      style={{
+        backgroundImage: `url(${bg})`,
+        backgroundSize: "cover",
+        backgroundPositionX: "center",
+      }}
+    >
+      <div className="flex w-[60%] bg-gray-100 dark:bg-primary sm:dark:bg-secondary px-0 md:px-[50px] md:mx-0 mx-5 rounded-lg mt-[30px] mb-[34px] md:flex-row gap-10 items-center">
+        
+        <div className="border bg-gray-100 p-10 rounded-lg dark:bg-secondary flex flex-col w-[80%] h-[100%]">
+         <div className=" flex flex-col items-center justify-center mb-4">
+          <img src={data.image ? data.image : avatar} alt='' className="mb-5 h-28 w-28 rounded-full" />
+          <h1 className="text-sky-500 font-semibold mb-5">{data.username}</h1>
+         </div>
+          <Link to="/">
+
+          <button  className="p-2 border drop-shadow-lg bg-gray-300 dark:bg-gray-800 text-gray-900 dark:text-white rounded-md w-full mb-4 hover:bg-sky-500 hover:text-white dark:hover:bg-sky-500">
+            Back to home
           </button>
+          </Link>
+          <button className="p-2 border drop-shadow-lg bg-gray-300 dark:bg-gray-800 text-gray-900 dark:text-white rounded-md w-full mb-4 hover:bg-sky-500  hover:text-white dark:hover:bg-sky-500">
+            Edit Profile
+          </button>
+          <button className="p-2 border drop-shadow-lg bg-gray-300 dark:bg-gray-800 text-gray-900 dark:text-white rounded-md w-full mb-4 hover:bg-sky-500  hover:text-white dark:hover:bg-sky-500">
+            Settings
+          </button>
+          <button className="p-2 border drop-shadow-lg bg-gray-300 dark:bg-gray-800 text-gray-900 dark:text-white rounded-md w-full mb-4 hover:bg-sky-500  hover:text-white dark:hover:bg-sky-500">
+            Favorites
+          </button>
+          <button className="p-2 border drop-shadow-lg bg-gray-300 dark:bg-gray-800 text-gray-900 dark:text-white rounded-md w-full hover:bg-sky-500  hover:text-white dark:hover:bg-sky-500">
+            Log out
+          </button>
+        </div>
+
+        <div className="w-full">
+          <div className="bg-gray-300 text-gray-900 dark:bg-primary rounded-lg p-8 flex flex-col md:mx-auto w-full my-16 relative">
+            <h2 className={`text-center text-sky-400 dark:text-gradient ${styles.heading3}  items-center justify-center m-5 mx-auto`}>Update Profile</h2>
+            {error && <p className="text-red-600">{`${error}`}<FaUserLock style={{ display: "inline-block", marginLeft: "10px" }} /></p>}
+            <div className="relative mb-4 w-full">
+              <label htmlFor="username" className="leading-8 text-sm text-gray-900 dark:text-white font-semibold">
+                Username
+              </label>
+              <input
+                value={data.username}
+                type="text"
+                id="username"
+                name="username"
+                placeholder="Enter username"
+                className={`drop-shadow-lg w-full bg-white rounded border ${errors.username ? "border-red-500" : "border-gray-300"
+                  } focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-900 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out z-0`}
+                onChange={handleInputs}
+                onKeyDown={handleKeyDown}
+                autoComplete="off"
+              />
+              {errors.username && <p className="text-red-500">{errors.username}</p>}
+            </div>
+            <div className="relative mb-4">
+              <label htmlFor="email" className="leading-8 text-sm text-gray-900 dark:text-white font-semibold">
+                Email
+              </label>
+              <input
+                value={data.email}
+                type="email"
+                id="email"
+                name="email"
+                placeholder="xyz@gmail.com"
+                className={`drop-shadow-lg w-full bg-white rounded border ${errors.email ? "border-red-500" : "border-gray-300"
+                  } focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-900 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out z-0`}
+                onChange={handleInputs}
+                onKeyDown={handleKeyDown}
+                autoComplete="off"
+              />
+              {errors.email && <p className="text-red-500">{errors.email}</p>}
+            </div>
+            <div className="relative mb-4">
+              <label htmlFor="contact" className="leading-8 text-sm text-gray-900 dark:text-white font-semibold">
+                Contact Number
+              </label>
+              <input
+                // value={data.contact}
+                type="contact"
+                id="contact"
+                name="contact"
+                placeholder="+91 12345 67890"
+                className={`drop-shadow-lg w-full bg-white rounded border ${errors.email ? "border-red-500" : "border-gray-300"
+                  } focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-900 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out z-0`}
+                onChange={handleInputs}
+                onKeyDown={handleKeyDown}
+                autoComplete="off"
+              />
+              {errors.email && <p className="text-red-500">{errors.email}</p>}
+            </div>
+            <div className="relative mb-4">
+              <label htmlFor="password" className="leading-8 text-sm text-gray-900 dark:text-white font-semibold">
+                Password
+              </label>
+              <input
+                value={data.password}
+                type={passwordType}
+                id="password"
+                name="password"
+                placeholder="Enter your password"
+                className={`drop-shadow-lg w-full bg-white rounded border ${errors.password ? "border-red-500" : "border-gray-300"
+                  } focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out z-0`}
+                onChange={handleInputs}
+                onKeyDown={handleKeyDown}
+              />
+              <button
+                onClick={passwordToggle}
+                className="absolute inset-y-0 right-0 top-7 pr-3 flex items-center pointer-events-cursor-pointer"
+              >
+                <img
+                  height={30}
+                  width={30}
+                  src={passwordType === "password" ? hide : show}
+                  alt="Toggle password visibility" loading='lazy'
+                />
+              </button>
+              {errors.password && (
+                <p className="text-red-500">{errors.password}</p>
+              )}
+            </div>
+            <button
+              className="text-black drop-shadow-lg hover:bg-indigo-500 bg-blue-gradient mt-2 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+              onClick={handleSubmit}
+            >
+              Update
+            </button>
+          </div>
         </div>
       </div>
     </section>
