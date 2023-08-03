c7df2803d6fee14fb4a695aded751dc50f3d5879
Anamika Yadav <aanyayadav419@gmail.com>
2023-08-04 01:44:35 +0530

    merge

--- src/components/Banner/MovieBanner.jsx
+++ src/components/Banner/MovieBanner.jsx
@@ -5,6 +5,9 @@ import { useParams } from "react-router-dom";
 import fetchData from "../../helper/fetchData";
 import styles from "../../style";
 import axios from "axios";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
+import Youtube from "react-youtube";
 import Details from "./Details";
 import CommonBanner from "./CommonBanner";
 
