3bdc845650fdf0ba5d94c23c53fb1c01231dd222
Bharti Vilas Nagpure <104631882+BhartiNagpure@users.noreply.github.com>
2023-08-10 09:03:31 +0530

    conflict resolve

--- src/components/Contributor.jsx
+++ src/components/Contributor.jsx
@@ -24,7 +24,7 @@ const Contributor = () => {
   const indexOfFirstUser = indexOfLastUser - usersPerPage;
   const currentUsers = contributors.slice(indexOfFirstUser, indexOfLastUser);
 
-  // Change page
+
   const paginate = (pageNumber) => {
     setCurrentPage(pageNumber);
   };
